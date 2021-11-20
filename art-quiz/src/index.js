0782cbf25503b0d4632c65861715fda9ff868791
”Stellarator85” <stellarator85@gmail.com>
2021-11-20 19:24:07 +0200

    Start project & upload create-rss-app-package

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -1,32 +1,12 @@
-//import image from "./images/lazy.png";
 
-const createImage = (src) =>
-  new Promise((res, rej) => {
-    const img = new Image();
-    img.onload = () => res(img);
-    img.onerror = rej;
-    img.src = src;
-  });
-/*
-async function render() {
-  const subHeader = document.createElement("h2");
-  subHeader.innerHTML = "This elements was created by js";
-  const myImage = await createImage(image);
-  document.body.appendChild(subHeader);
-  document.body.appendChild(myImage);
-}
 
-render();
-*/
 
+
+import path from "path";
 
 //document.querySelector("h1").contains("")&& document.querySelector("h2")
 
 
-
-import { promises } from "fs";
-
-import { dirname } from "path";
 
 /*       
 var img = document.createElement("img");
